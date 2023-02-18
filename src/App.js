b337ab0c21e95d1e0fa55fce573ecb483da73847
Ramesh Manepalli <ramesh.manepalli@lifesight.io>
2023-02-18 08:18:08 +0530

    removed right click event listener

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import './App.css';
 import Dashboard from "./components/dashboard/Dashboard";
+import {useEffect} from "react";
 
 const App = () => {
+
+    useEffect(() => {
+        const handleContextmenu = e => {
+            e.preventDefault()
+        }
+        document.addEventListener('contextmenu', handleContextmenu)
+        return function cleanup() {
+            document.removeEventListener('contextmenu', handleContextmenu)
+        }
+    }, [ ])
+
     return (
         <div className='App'>
          <Dashboard/>
