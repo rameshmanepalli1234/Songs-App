d72f3e0de710539bbb95e61ed1698d78f8949758
Ramesh Manepalli <ramesh.manepalli@lifesight.io>
2023-02-19 12:48:36 +0530

    dfjdas

--- src/components/jsFiles/dailyVerses.js
+++ src/components/jsFiles/dailyVerses.js
@@ -7,17 +7,122 @@ export const dailyVerses = [
         date: '19/02/2023'
     },
     {
-        englishVerse: 'hi',
-        teluguVerse: '',
-        englishChapter: '',
-        teluguChapter: '',
+        englishVerse: 'And I will bless them that bless thee, and curse him that curseth thee: and in thee shall all families of the earth be blessed.',
+        teluguVerse: 'నిన్ను ఆశీర్వదించువారిని ఆశీర్వదించెదను; నిన్ను దూషించువాని శపించెదను; భూమియొక్క సమస్తవంశ ములు నీయందు ఆశీర్వదించబడునని అబ్రాముతో అనగా',
+        englishChapter: 'Genesis 12:3',
+        teluguChapter: 'ఆదికాండము 12:3',
         date: '20/02/2023'
     },
     {
-        englishVerse: 'hello hi',
+        englishVerse: 'And I am come down to deliver them out of the hand of the Egyptians, and to bring them up out of that land unto a good land and a large, unto a land flowing with milk and honey; unto the place of the Canaanites, and the Hittites, and the Amorites, and the Perizzites, and the Hivites, and the Jebusites.',
+        teluguVerse: 'కాబట్టి ఐగుప్తీయుల చేతిలోనుండి వారిని విడిపించుటకును, ఆ దేశములోనుండి విశాలమైన మంచి దేశమునకు, అనగా కనానీయులకు హిత్తీయులకు అమోరీయులకు పెరిజ్జీయు లకు హివ్వీయులకు యెబూసీయులకు నివాసస్థానమై, పాలు తేనెలు ప్రవహించు దేశమునకు వారిని నడిపించు టకును దిగివచ్చి యున్నాను.',
+        englishChapter: 'Exodus 3:8',
+        teluguChapter: 'నిర్గమకాండము 3:8',
+        date: '21/02/2023'
+    },
+    {
+        englishVerse: 'And when the day of Pentecost was fully come, they were all with one accord in one place.',
+        teluguVerse: 'పెంతెకొస్తను పండుగదినము వచ్చినప్పుడు అందరు ఒకచోట కూడియుండిరి',
+        englishChapter: 'Acts 2:1',
+        teluguChapter: 'అపొస్తలుల కార్యములు 2:1',
+        date: '22/02/2023'
+    },
+    {
+        englishVerse: 'The LORD will give strength unto his people; the LORD will bless his people with peace.',
+        teluguVerse: 'యెహోవా తన ప్రజలకు బలము ననుగ్రహించును యెహోవా తన ప్రజలకు సమాధానము కలుగజేసి వారి నాశీర్వదించును.',
+        englishChapter: 'Psalms 29:11',
+        teluguChapter: 'కీర్తనల గ్రంథము 29:11',
+        date: '23/02/2023'
+    },
+    {
+        englishVerse: 'Then answered I them, and said unto them, The God of heaven, he will prosper us; therefore we his servants will arise and build: but ye have no portion, nor right, nor memorial, in Jerusalem.',
+        teluguVerse: 'అందుకు నేనుఆకాశమందు నివాసియైన దేవుడు తానే మా యత్నమును సఫలము చేయును గనుక ఆయన దాసులమైన మేము కట్టుటకు పూనుకొనుచున్నాము, యెరూషలేమునందు మీకు భాగమైనను స్వతంత్రమైనను జ్ఞాపక సూచనయైనను లేదని ప్రత్యుత్తరమిచ్చితిని.',
+        englishChapter: 'Nehemiah 2:20',
+        teluguChapter: 'నెహెమ్యా 2:20',
+        date: '24/02/2023'
+    },
+    {
+        englishVerse: '',
         teluguVerse: '',
         englishChapter: '',
         teluguChapter: '',
-        date: '21/02/2023'
+        date: '25/02/2023'
+    },
+    {
+        englishVerse: '',
+        teluguVerse: '',
+        englishChapter: '',
+        teluguChapter: '',
+        date: '26/02/2023'
+    },
+    {
+        englishVerse: '',
+        teluguVerse: '',
+        englishChapter: '',
+        teluguChapter: '',
+        date: '27/02/2023'
+    },
+    {
+        englishVerse: '',
+        teluguVerse: '',
+        englishChapter: '',
+        teluguChapter: '',
+        date: '28/02/2023'
+    },
+    {
+        englishVerse: '',
+        teluguVerse: '',
+        englishChapter: '',
+        teluguChapter: '',
+        date: '01/03/2023'
+    },
+    {
+        englishVerse: '',
+        teluguVerse: '',
+        englishChapter: '',
+        teluguChapter: '',
+        date: '02/03/2023'
+    },
+    {
+        englishVerse: '',
+        teluguVerse: '',
+        englishChapter: '',
+        teluguChapter: '',
+        date: '03/03/2023'
+    },
+    {
+        englishVerse: '',
+        teluguVerse: '',
+        englishChapter: '',
+        teluguChapter: '',
+        date: '04/03/2023'
+    },
+    {
+        englishVerse: '',
+        teluguVerse: '',
+        englishChapter: '',
+        teluguChapter: '',
+        date: '05/03/2023'
+    },
+    {
+        englishVerse: '',
+        teluguVerse: '',
+        englishChapter: '',
+        teluguChapter: '',
+        date: '06/03/2023'
+    },
+    {
+        englishVerse: '',
+        teluguVerse: '',
+        englishChapter: '',
+        teluguChapter: '',
+        date: '07/03/2023'
+    },
+    {
+        englishVerse: '',
+        teluguVerse: '',
+        englishChapter: '',
+        teluguChapter: '',
+        date: '08/03/2023'
     }
 ]
