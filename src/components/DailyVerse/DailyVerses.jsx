135d08b5f3bebaf13be120d225c2587f5926df2d
Ramesh Manepalli <ramesh.manepalli@lifesight.io>
2023-02-19 09:06:50 +0530

    added time stamp in daily verse in app

--- src/components/DailyVerse/DailyVerses.jsx
+++ src/components/DailyVerse/DailyVerses.jsx
@@ -3,18 +3,32 @@ import {BiX} from "react-icons/bi";
 import {Button} from "react-bootstrap";
 import dailyVerse from '../Images/dailyVerse.jpeg';
 import '../DailyVerse/DailyVerses.scss';
+import {useEffect, useState} from "react";
 
 const DailyVerses = (props) => {
 
     const { handleOnCloseDailyVerse,verse } = props;
+    const [time, setTime] = useState('');
+
+    useEffect(()=>{
+        setTimeout(()=>{
+            let timeUpdate = new Date().toLocaleTimeString();
+            setTime(timeUpdate);
+        },1000);
+    },[time]);
 
     return (
         <div>
             <Modal isOpen={true} centered className='dailyVerseModal'>
                 <ModalHeader className='customerServiceModalHeader'>
                     <img src={dailyVerse} alt='daily verse' height={100} width={100}/>
-                    <span>Verse of the Day, for {verse.date} </span>
-                    <BiX size={24} className='customerServiceCloseIcon' onClick={handleOnCloseDailyVerse}/>
+                    <span>Verse of the Day </span>
+                    <br/>
+                    <div className='dateTime_daily_verse'>
+                        <span>{new Date().toLocaleDateString()}</span>
+                        <span className='time_daily_verse'>{time}</span>
+                    </div>
+                    <BiX size={24} className='close_icon_daily_verse' onClick={handleOnCloseDailyVerse}/>
                 </ModalHeader>
                 <ModalBody>
                     <div>
