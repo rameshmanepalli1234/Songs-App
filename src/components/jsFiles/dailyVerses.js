322189131478c4de9d9452e6a96334b295fe150e
Ramesh Manepalli <ramesh.manepalli@lifesight.io>
2023-02-25 20:01:21 +0530

    updated daily verse js file

--- src/components/jsFiles/dailyVerses.js
+++ src/components/jsFiles/dailyVerses.js
@@ -56,73 +56,115 @@ export const dailyVerses = [
         date: '26/02/2023'
     },
     {
-        englishVerse: '',
-        teluguVerse: '',
-        englishChapter: '',
-        teluguChapter: '',
+        englishVerse: 'Go up to the mountain, and bring wood, and build the house; and I will take pleasure in it, and I will be glorified, saith the LORD.',
+        teluguVerse: 'పర్వతములెక్కి మ్రాను తీసికొని వచ్చి మీరు ఈ మందిరమును కట్టించినయెడల దానియందు నేను సంతోషించి నన్ను ఘనపరచుకొందునని యెహోవా సెలవిచ్చుచున్నాడు.',
+        englishChapter: 'Haggai 1:8',
+        teluguChapter: 'హగ్గయి 1:8',
         date: '27/02/2023'
     },
     {
-        englishVerse: '',
-        teluguVerse: '',
-        englishChapter: '',
-        teluguChapter: '',
+        englishVerse: 'And the LORD will take away from thee all sickness, and will put none of the evil diseases of Egypt, which thou knowest, upon thee; but will lay them upon all them that hate thee.',
+        teluguVerse: 'యెహోవా నీయొద్దనుండి సర్వరోగములను తొలగించి, నీవెరిగియున్న ఐగుప్తు లోని కఠినమైన క్షయ వ్యాధులన్నిటిని నీకు దూరపరచి, నిన్ను ద్వేషించు వారందరిమీదికే వాటిని పంపించును.',
+        englishChapter: 'Deuteronomy 7:15',
+        teluguChapter: 'ద్వితీయోపదేశ కాండము 7:15',
         date: '28/02/2023'
     },
     {
-        englishVerse: '',
-        teluguVerse: '',
-        englishChapter: '',
-        teluguChapter: '',
+        englishVerse: 'For he that is mighty hath done to me great things; and holy is his name.',
+        teluguVerse: 'సర్వశక్తిమంతుడు నాకు గొప్పకార్యములు చేసెను గనుక ఇది మొదలుకొని అన్నితరములవారును నన్ను ధన్యురాలని యందురు. ఆయన నామము పరిశుద్ధము.',
+        englishChapter: 'Luke 1:49',
+        teluguChapter: 'లూకా 1:49',
         date: '01/03/2023'
     },
     {
-        englishVerse: '',
-        teluguVerse: '',
-        englishChapter: '',
-        teluguChapter: '',
+        englishVerse: 'Hatred stirreth up strifes: but love covereth all sins.',
+        teluguVerse: 'పగ కలహమును రేపును ప్రేమ దోషములన్నిటిని కప్పును.',
+        englishChapter: 'Proverbs ',
+        teluguChapter: 'సామెతలు 10:12',
         date: '02/03/2023'
     },
     {
-        englishVerse: '',
-        teluguVerse: '',
-        englishChapter: '',
-        teluguChapter: '',
+        englishVerse: 'All we like sheep have gone astray; we have turned every one to his own way; and the LORD hath laid on him the iniquity of us all.',
+        teluguVerse: 'మనమందరము గొఱ్ఱలవలె త్రోవ తప్పిపోతివిు మనలో ప్రతివాడును తనకిష్టమైన త్రోవకు తొలిగెను యెహోవా మన యందరి దోషమును అతనిమీద మోపెను.',
+        englishChapter: 'Isaiah 53:6',
+        teluguChapter: 'యెషయా 53:6',
         date: '03/03/2023'
     },
     {
-        englishVerse: '',
-        teluguVerse: '',
-        englishChapter: '',
-        teluguChapter: '',
+        englishVerse: 'Whereunto he called you by our gospel, to the obtaining of the glory of our Lord Jesus Christ.',
+        teluguVerse: 'మీరీలాగున రక్షింపబడి మన ప్రభువైన యేసుక్రీస్తుయొక్క మహిమను పొందవలెనని, ఆయన మా సువార్త వలన మిమ్మును పిలిచెను.',
+        englishChapter: '2 Thessalonians 2:14',
+        teluguChapter: '2 థెస్సలొనీయులకు 2:14',
         date: '04/03/2023'
     },
     {
-        englishVerse: '',
-        teluguVerse: '',
-        englishChapter: '',
-        teluguChapter: '',
+        englishVerse: 'Ask of me, and I shall give thee the heathen for thine inheritance, and the uttermost parts of the earth for thy possession.',
+        teluguVerse: 'నన్ను అడుగుము, జనములను నీకు స్వాస్థ్యముగానుభూమిని దిగంతములవరకు సొత్తుగాను ఇచ్చెదను.',
+        englishChapter: 'Psalms 2:8',
+        teluguChapter: 'కీర్తనల గ్రంథము 2:8',
         date: '05/03/2023'
     },
     {
-        englishVerse: '',
-        teluguVerse: '',
-        englishChapter: '',
-        teluguChapter: '',
+        englishVerse: 'Now when all the people were baptized, it came to pass, that Jesus also being baptized, and praying, the heaven was opened,And the Holy Ghost descended in a bodily shape like a dove upon him, and a voice came from heaven, which said, Thou art my beloved Son; in thee I am well pleased.',
+        teluguVerse: 'ప్రజలందరును బాప్తిస్మము పొందినప్పుడు యేసుకూడ బాప్తిస్మము పొంది ప్రార్థన చేయుచుండగా ఆకాశము తెరవబడి పరిశుద్ధాత్మ శరీరాకారముతో పావురమువలె ఆయనమీదికి దిగి వచ్చెను. అప్పుడునీవు నా ప్రియ కుమారుడవు, నీయందు నేనానందించుచున్నానని యొక శబ్దము ఆకాశమునుండి వచ్చెను.',
+        englishChapter: 'Luke 3:21',
+        teluguChapter: 'లూకా 3:21',
         date: '06/03/2023'
     },
     {
-        englishVerse: '',
-        teluguVerse: '',
-        englishChapter: '',
-        teluguChapter: '',
+        englishVerse: 'And they cast out many devils, and anointed with oil many that were sick, and healed them.',
+        teluguVerse: 'అనేక దయ్యములు వెళ్లగొట్టుచు నూనెరాచి అనేకులగు రోగులను స్వస్థపరచుచునుండిరి.',
+        englishChapter: 'Mark 6:13',
+        teluguChapter: 'మార్కు 6:13',
         date: '07/03/2023'
     },
     {
-        englishVerse: '',
-        teluguVerse: '',
-        englishChapter: '',
-        teluguChapter: '',
+        englishVerse: 'Now therefore, if ye will obey my voice indeed, and keep my covenant, then ye shall be a peculiar treasure unto me above all people: for all the earth is mine:',
+        teluguVerse: 'కాగా మీరు నా మాట శ్రద్ధగా విని నా నిబంధన ననుసరించి నడిచినయెడల మీరు సమస్తదేశ జనులలో నాకు స్వకీయ సంపాద్య మగు దురు.',
+        englishChapter: 'Exodus 19:5',
+        teluguChapter: 'నిర్గమకాండము 19:5',
         date: '08/03/2023'
-    }
+    },
+{
+    englishVerse: 'Every day will I bless thee; and I will praise thy name for ever and ever',
+    teluguVerse: 'అనుదినము నేను నిన్ను స్తుతించెదను నిత్యము నీ నామమును స్తుతించెదను',
+    englishChapter: 'Psalms 145:2',
+    teluguChapter: 'కీర్తనల గ్రంథము 145:2',
+    date: '09/03/2023'
+},
+{
+    englishVerse: 'There is none like unto the God of Jeshurun, who rideth upon the heaven in thy help, and in his excellency on the sky.',
+    teluguVerse: 'యెషూరూనూ, దేవుని పోలినవాడెవడును లేడు ఆయన నీకు సహాయము చేయుటకు ఆకాశవాహనుడై వచ్చును మహోన్నతుడై మేఘవాహనుడగును.',
+    englishChapter: 'Deuteronomy 33:26',
+    teluguChapter: 'ద్వితీయోపదేశ కాండము 33:26',
+    date: '10/03/2023'
+},
+{
+    englishVerse: '',
+    teluguVerse: '',
+    englishChapter: '',
+    teluguChapter: '',
+    date: '11/03/2023'
+},
+{
+    englishVerse: '',
+    teluguVerse: '',
+    englishChapter: '',
+    teluguChapter: '',
+    date: '12/03/2023'
+},
+{
+    englishVerse: '',
+    teluguVerse: '',
+    englishChapter: '',
+    teluguChapter: '',
+    date: '13/03/2023'
+},
+{
+    englishVerse: '',
+    teluguVerse: '',
+    englishChapter: '',
+    teluguChapter: '',
+    date: '14/03/2023'
+}
 ]
